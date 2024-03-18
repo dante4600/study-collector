acf4cd4cd57096e068d0cf8907a4ce9f63e3cab4
dante4600 <99743709+dante4600@users.noreply.github.com>
2024-03-18 16:18:34 +0100

    Create Application component skeletons

---
+++ components/StudyApplicationService/src/main/java/hu/example/StudyApplicationService/StudyApplicationServiceApplication.java
@@ -0,0 +1,13 @@
+package hu.example.StudyApplicationService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyApplicationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyApplicationServiceApplication.class, args);
+	}
+
+}
---
+++ components/StudyApplicationService/src/test/java/hu/example/StudyApplicationService/StudyApplicationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.example.StudyApplicationService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyApplicationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
